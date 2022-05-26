0040955c505c830898d6ac72a348a761f18c781b
RanjithSivam <ranjithsivam16@gmail.com>
2022-05-26 20:18:46 +0530

    added frontend

--- src/main/java/com/ranjith/webapp/controller/GroupsController.java
+++ src/main/java/com/ranjith/webapp/controller/GroupsController.java
@@ -16,34 +16,106 @@ import com.ranjith.webapp.model.Users;
 import com.ranjith.webapp.service.GroupsService;
 import com.ranjith.webapp.service.GroupsServiceImp;
 
-@WebServlet(urlPatterns = "/groups")
+@WebServlet(urlPatterns = "/groups/*")
 public class GroupsController extends HttpServlet{
 	
-	private Gson gson = new Gson();
-	private GroupsService groupsService = new GroupsServiceImp();
+	private Gson gson;
+	private GroupsService groupsService;
+
+	@Override
+	public void init() throws ServletException {
+		gson = new Gson();
+		groupsService = new GroupsServiceImp();
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter out = resp.getWriter();
-		resp.setContentType("application/json");
-		String id = req.getParameter("groupId");
-		if(id!=null) {
-			List<Users> list = groupsService.findAllUsersInGroups(Integer.parseInt(id));
-			out.print(gson.toJson(list));
-			System.out.println(list);
-			out.close();
-		}else {
-			List<Groups> list = groupsService.findAllGroups();
-			out.print(gson.toJson(list));
-			out.close();
+		
+		String path = req.getPathInfo();
+		
+		switch(path) {
+		case "/all":
+			showAllGroups(req,resp);
+			break;
+		case "/form":
+			showForm(req,resp);
+			break;
+		case "/user":
+			showAllUserInGroup(req,resp);
+			break;
+			
 		}
+		
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		
 		String name = req.getParameter("name");
-		System.out.println(name);
 		groupsService.addGroup(name);
+		
+		out.println("<h2>Group Created!!</h2>");
 	}
+	
+	private void showAllGroups(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		
+		List<Groups> list = groupsService.findAllGroups();
+		
+		out.println("<table><thead><tr><th>ID</th><th>Name</th></tr></thead><tbody>");
+		
+		for(Groups group:list) {
+			out.println("<tr>");
+			out.println("<td>"+group.getId() +"</td>");
+			out.println("<td>"+group.getName() +"</td>");
+			out.println("</tr>");
+		}
+		
+		out.println("</tbody></table>");
+		out.close();
+	}
+	
+	private void showAllUserInGroup(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		
+		String id = req.getParameter("groupId");
+		List<Users> list = groupsService.findAllUsersInGroups(Integer.parseInt(id));
+		
+		out.println("<table><thead><tr><th>ID</th><th>Name</th></tr></thead><tbody>");
+		
+		for(Users user:list) {
+			out.println("<tr>");
+			out.println("<td>"+user.getId() +"</td>");
+			out.println("<td>"+user.getName() +"</td>");
+			out.println("</tr>");
+		}
+		
+		out.println("</tbody></table>");
+		out.close();
+	}
+	
+	private void showForm(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("text/html");
+		out.println("<h4>Add New Group</h4>");
+		out.println("<form method=\"post\" action=\"\">"
+				+ "  <div>\n"
+				+ "    <label>Name:</label>\n"
+				+ "    <input type=\"text\" name=\"name\" required />\n"
+				+ "  </div>\n"
+				+ "  <button>\n"
+				+ "    submit\n"
+				+ "  </button>\n"
+				+ "</form>");
+		out.close();
+	}
+
 	
 }
--- src/main/java/com/ranjith/webapp/controller/UsersController.java
+++ src/main/java/com/ranjith/webapp/controller/UsersController.java
@@ -12,22 +12,35 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.ranjith.webapp.model.Users;
+import com.ranjith.webapp.service.GroupsServiceImp;
 import com.ranjith.webapp.service.UsersService;
 import com.ranjith.webapp.service.UsersServiceImp;
 
-@WebServlet(urlPatterns = "/users")
+@WebServlet(urlPatterns = "/users/*")
 public class UsersController extends HttpServlet{
 	
-	private Gson gson = new Gson();
-	private UsersService userService = new UsersServiceImp();
+	private Gson gson;
+	private UsersService userService;
+	
+	@Override
+	public void init() throws ServletException {
+		gson = new Gson();
+		userService = new UsersServiceImp();
+	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter out = resp.getWriter();
-		resp.setContentType("application/json");
-		List<Users> list = userService.findAllUsers();
-		out.print(gson.toJson(list));
-		out.close();
+		String path = req.getPathInfo();
+		
+		switch(path) {
+		case "/all":
+			showAllUsers(req,resp);
+			break;
+		case "/form":
+			showForm(req,resp);
+			break;
+			
+		}
 	}
 
 	@Override
@@ -39,6 +52,67 @@ public class UsersController extends HttpServlet{
 		String gender = req.getParameter("gender");
 		
 		userService.addUser(new Users(null,name,email,phone,gender));
+		
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<h2>Registeration Successfull!!!</h2>");
 	}
 	
+	private void showAllUsers(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		List<Users> list = userService.findAllUsers();
+		
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<table><thead><tr><th>ID</th><th>Name</th><th>Email</th><th>Gender</th><th>Phone</th></tr></thead><tbody>");
+		
+		for(Users user:list) {
+			out.println("<tr>");
+			out.println("<td>"+user.getId() +"</td>");
+			out.println("<td>"+user.getName() +"</td>");
+			out.println("<td>"+user.getEmail() +"</td>");
+			out.println("<td>"+user.getGender() +"</td>");
+			out.println("<td>"+user.getPhone() +"</td>");
+			out.println("</tr>");
+		}
+		
+		out.println("</tbody></table>");
+		out.close();
+	}
+	
+	private void showForm(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("text/html");
+		out.println("<h4>User Registeration Form</h4>");
+		out.println("<form method=\"post\" action=\"\">"
+				+ "  <div>\n"
+				+ "    <label>Name:</label>\n"
+				+ "    <input type=\"text\" name=\"name\" required />\n"
+				+ "  </div>\n"
+				+ "  <div>\n"
+				+ "    <label>email:</label>\n"
+				+ "    <input type=\"text\" name=\"email\" required/>\n"
+				+ "  </div>\n"
+				+ "  <div>\n"
+				+ "    <small>\n"
+				+ "      Gender\n"
+				+ "    </small>\n"
+				+ "    <input type=\"radio\" value=\"Male\" name=\"gender\" required />\n"
+				+ "    <label>Male</label>\n"
+				+ "    \n"
+				+ "    <input type=\"radio\" value=\"Female\" name=\"gender\"/>\n"
+				+ "    <label>Female</label>\n"
+				+ "  </div>\n"
+				+ "  <div>\n"
+				+ "    <label>Phone:</label>\n"
+				+ "    <input type=\"text\" name=\"phone\" required/>\n"
+				+ "  </div>\n"
+				+ "  <button>\n"
+				+ "    submit\n"
+				+ "  </button>\n"
+				+ "</form>");
+		out.close();
+	}
 }
